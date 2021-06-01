913ffcac6d25ee5ce82b3dce8f60254907200e17
Elvan Aydemir <aelvanaydemir@gmail.com>
2021-06-01 13:51:00 +0000

    Parser and downloader classes separated

--- sebra_pipeline.py
+++ sebra_pipeline.py
@@ -14,7 +14,11 @@ import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-class SebraParser:
+import pandas as pd
+import numpy as np
+import os
+
+class SebraDownloader:
     def __init__(self, file_location, chrome_path, folders, min_year, min_month,min_day) -> None:
         
         self.path = file_location
@@ -23,6 +27,7 @@ class SebraParser:
         self.min_month = min_month
         self.min_day = min_day
         self.folders = folders
+        
 
         self.issues_requests = {}
         self.issues_download = {}
@@ -147,10 +152,13 @@ class SebraParser:
                                 processed_files =+ 1
                                 url = file
                                 if "NF" in file:
+                                        self.parent_location = self.path + self.folders[1]
                                         location  = self.path + self.folders[1] + '/' + year
                                 elif "MF" in file:
+                                        self.parent_location = self.path + self.folders[2]
                                         location  = self.path + self.folders[2] + '/' + year
-                                else:  
+                                else:
+                                        self.parent_location = self.path + self.folders[0]  
                                         location  = self.path + self.folders[0] + '/' + year
 
                                 file_name = [r.end() for r in re.finditer('/',file)][-1] - 1
@@ -188,22 +196,6 @@ class SebraParser:
         with open(self.path + '/'+ name, 'wb') as config_dictionary_file:
             pickle.dump(dates_meta, config_dictionary_file)
 
-    def read_files(self):
-        """
-        Reads downloaded report files from local directory
-
-        [extended_summary]
-        """
-        pass
-
-    def parse_files(self):
-        """
-        Parses the individual files into an Excel
-
-        [extended_summary]
-        """
-        pass
-
     def upload_to_gc(self, remove_from_local = True):
         """
         Uploads raw reports and the combined data into relevant GCloud folder
@@ -216,14 +208,202 @@ class SebraParser:
         pass
 
 
+class SebraParser:
+    def __init__(self, parent_folder):
+        self.excel_file_path = parent_folder
+
+        def read_into_df(self):
+            self.df = pd.read_excel(self.excel_file_path)
+
+        # def parse_sebra_excel_file_to_pandas(self):
+        def find_organizations_start_row(self):
+            org_start_phrases = [
+                'ПЛАЩАНИЯ ПО ПЪРВОСТЕПЕННИ СИСТЕМИ В СЕБРА ',
+                'ПЛАЩАНИЯ ОТ БЮДЖЕТА, ИЗВЪРШЕНИ ЧРЕЗ СЕБРА, ПО ПЪРВОСТЕПЕННИ СИСТЕМИ'
+            ]
+            correct = False
+            for t in org_start_phrases:
+                mask = self.df.iloc[:, 0] == t
+                if mask.sum() == 1:
+                    correct = True
+                    break
+            assert correct, f'There should be exactly one row containing any of the phrases {org_start_phrases}. File "{excel_file_path}".'
+            return mask[mask].index[0]
+        
+        def find_operations_blocks(self, org_start_row):
+            op_start_text = 'Описание'
+            op_end_text = 'Общо:'
+            mask_start = self.df.iloc[:, 1] == op_start_text
+            mask_end = self.df.iloc[:, 0].str.startswith(op_end_text).fillna(False)
+            op_start_index = mask_start[mask_start].index
+            op_end_index = mask_end[mask_end].index
+            
+            if not (len(op_start_index) == len(op_end_index)):
+                print(op_start_index, op_end_index)
+                # display(df)
+                
+            assert len(op_start_index) == len(op_end_index), f'The number of occurances of "{op_start_text}" is not the same as the number of occurances of "{op_end_text}". File "{excel_file_path}".'
+            op_blocks = list(zip(op_start_index, op_end_index))
+            op_blocks_dict = {
+                'general': [],
+                'organizations': []
+            }
+            for i in range(len(op_blocks)):
+                b_start, b_end = op_blocks[i]
+                assert b_start < b_end, f'Block start index ({b_start}) should be smaller that the end index ({b_end}). File "{self.excel_file_path}".'
+                if i > 0:
+                    _, prev_b_end = op_blocks[i - 1]
+                    assert prev_b_end < b_start, f'Block end index ({prev_b_end}) of the previous block should be smaller that the start index of the next block ({b_start}). File "{self.excel_file_path}".'
+                assert not (b_start < org_start_row and org_start_row < b_end), f'The starting row for organizations shouldn\'t be within an operations block. File "{self.excel_file_path}".'
+
+                if b_end < org_start_row:
+                    op_blocks_dict['general'].append((b_start, b_end))
+                else:
+                    op_blocks_dict['organizations'].append((b_start, b_end))
+
+                assert len(op_blocks_dict['general']) == 1, 'There should be exactly one block for general totals. File "{excel_file_path}".'
+
+            return op_blocks_dict
+
+        def get_general_totals(self, general_op_block):
+            new_columns = ['Operations Code', 'Operations Description', 'Operations Count', 'Operations Amount (BGN)']
+            b_start, b_end = general_op_block
+            block_df = self.df.iloc[b_start + 1: b_end].copy()
+            block_df.columns = new_columns
+            del block_df['Operations Count']
+
+            # drop all rows that do not have amount value
+            block_df = block_df[~block_df['Operations Amount (BGN)'].isna()]
+
+            block_df['Operations Amount (BGN)'] = block_df['Operations Amount (BGN)'] \
+                .astype(str) \
+                .str.replace(' ', '') \
+                .str.replace(',', '.') \
+                .astype('float64')
+            block_df = block_df.set_index('Operations Code')
+
+            # check if sums are correct
+            sum_row = self.df.iloc[b_end, :]
+            sum_row.index = new_columns
+            if isinstance(sum_row['Operations Amount (BGN)'], str):
+                sum_row['Operations Amount (BGN)'] = float(
+                    sum_row['Operations Amount (BGN)'] \
+                        .replace(' ', '') \
+                        .replace(',', '.')
+                )
+            if not round(sum_row['Operations Amount (BGN)'], 2) == round(block_df['Operations Amount (BGN)'].sum(), 2):
+                print(f"Warning: The sums of \"Operations Amount (BGN)\" do not match for block ({b_start}, {b_end}). Expected value \"{round(sum_row['Operations Amount (BGN)'], 2)}\". Calculated value \"{round(block_df['Operations Amount (BGN)'].sum(), 2)}\". File \"{self.excel_file_path}\".")
+
+            return block_df
+
+        def get_organization_name_and__period(self, b_start):
+            block_header = self.df.iloc[b_start - 1]
+            org_name = block_header[0]
+            if isinstance(block_header[2], str):
+                # this is the standard case - the header is just one row
+                period = block_header[2].split(' ')
+            else:
+                # this is a rare case where the header takes two rows
+                block_header_1 = self.df.iloc[b_start - 2]
+                # concat the organization name along the two rows
+                assert isinstance(block_header_1[0], str), f'The first column does not contain the organization name around row {b_start} in file "{self.excel_file_path}".'
+                org_name = block_header_1[0] + ' ' + org_name
+                assert isinstance(block_header_1[2], str), f'The third column does not contain the period string around row {b_start} in file "{self.excel_file_path}".'
+                period = block_header_1[2].split(' ')
+            assert period[0] == 'Период:', f'Field for time period should start with string "Период". File "{self.excel_file_path}".'
+            start_date = period[1]
+            end_date = period[3]
+            return org_name, start_date, end_date
+
+        def get_organization_operations_blocks(self, org_op_blocks):
+            
+        
+            new_columns = ['Operations Code', 'Operations Description', 'Operations Count', 'Operations Amount (BGN)']
+            org_op_blocks_dfs = []
+            for b_start, b_end in org_op_blocks:
+                org_df = self.df.iloc[b_start + 1: b_end, :].copy()
+                assert not org_df.iloc[:, 1].isna().any(), f'There are empty cells within the operations block between rows ({b_start}, {b_end}). File "{self.excel_file_path}".'
+                org_df.columns = new_columns
+                del org_df['Operations Count']
+                org_df['Operations Amount (BGN)'] = org_df['Operations Amount (BGN)'] \
+                    .astype(str) \
+                    .str.replace(' ', '') \
+                    .str.replace(',', '.') \
+                    .astype('float64')
+
+                # get organization name and time period
+                org_name, start_date, end_date = self.get_organization_name_and__period(b_start)
+                org_df['Organization Name'] = org_name
+                org_df['Start Date'] = start_date
+                org_df['End Date'] = end_date
+
+                # check if sums are correct
+                sum_row = df.iloc[b_end, :]
+                sum_row.index = new_columns
+                if isinstance(sum_row['Operations Amount (BGN)'], str):
+                    sum_row['Operations Amount (BGN)'] = float(
+                        sum_row['Operations Amount (BGN)'] \
+                            .replace(' ', '') \
+                            .replace(',', '.')
+                    )
+                if not round(sum_row['Operations Amount (BGN)'], 2) == round(org_df['Operations Amount (BGN)'].sum(), 2):
+                    print(f"Warning: The sums of \"Operations Amount (BGN)\" do not match for block ({b_start}, {b_end}). Expected value \"{round(sum_row['Operations Amount (BGN)'], 2)}\". Calculated value \"{round(org_df['Operations Amount (BGN)'].sum(), 2)}\". File \"{self.excel_file_path}\".")
+
+                org_op_blocks_dfs.append(org_df)
+            return pd.concat(org_op_blocks_dfs).reset_index().drop(columns=['index'])
+
+        def check_sums(self, ops_df, general_block_df):
+            ops_totals_df = ops_df.groupby('Operations Code').sum()
+            
+            assert (ops_df['Operations Code'].dtypes == object) and (general_block_df.reset_index()['Operations Code'].dtypes == object), f'There are missing operation codes in file "{excel_file_path}".'
+            
+            sum_check_df = pd.merge(
+                ops_totals_df,
+                general_block_df,
+                how='left',
+                left_on='Operations Code',
+                right_index=True
+            )
+            if not (sum_check_df['Operations Amount (BGN)_x'].round(2) == sum_check_df['Operations Amount (BGN)_y'].round(2)).all():
+                print(f'Warning: Some sums do not match Operations Amount (BGN). File "{self.excel_file_path}".')
+                
+               
+        def run_parser(self):
+            # get the row that indicates the start of the organizations section
+            org_start_row = find_organizations_start_row(self.df)
+            
+            # get the start and end rows for all blocks of operations
+            op_blocks = find_operations_blocks(self.df, org_start_row)
+            
+            # parse the data containing the general totals for all operations in the file
+            # (we will use those just to check the sums at the end for correctness)
+            general_block_df = get_general_totals(self.df, op_blocks['general'][0])
+            
+            # parse all operations by organization in the file
+            ops_df = get_organization_operations_blocks(self.df, op_blocks['organizations'])
+            
+            # check if the sums for all opearions by organization match the general totals
+            check_sums(ops_df, general_block_df)
+
+            # extract the Organization ID from the Name
+            ops_df['Organization ID'] = ops_df['Organization Name'].str.findall(r'\((.*?)\)').map(
+                lambda x: x[-1].strip() if len(x) > 0 else np.nan
+            )
+            
+            return ops_df
+
+
 def main():
     file_loc = './downloaded_files'
     folders = ['/SEBRA']#['/SEBRA','/NF_SEBRA','/MF_SEBRA']
-    min_year = 2021
-    min_month = 5
-    min_day = 15
+    min_year = 2020
+    min_month = 1
+    min_day = 1
     chrome_path = '/usr/local/bin/chromedriver'
-    sp = SebraParser(file_loc, chrome_path, folders, min_year, min_month, min_day)
 
-    sp.get_urls()
-    sp.download_reports()
+    sd = SebraDownloader(file_loc, chrome_path, folders, min_year, min_month, min_day)
+    sd.get_urls()
+    sd.download_reports()
+
+    sp = SebraParser(sd.parent_location)
+    ops_df = sp.run_parser()
