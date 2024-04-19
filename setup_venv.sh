6d9ccb0f25b91da5af4e9082914fad685a93946d
Yasen Petrov <yasenpetr@gmail.com>
2024-04-19 13:47:43 +0300

    Add shell script to setup venv
    We want to set up a virtualenv and install the sebra-scrape package
    inside of it such that this venv can be used for our DAG tasks

---
+++ setup_venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # exit if any command fails. Makes sure we don't pip install outside venv
+
+# create and activate a virtualenv inside a `venv` folder
+python -m venv venv
+source venv/bin/activate
+
+# install package defined in current project in venv
+pip install .
